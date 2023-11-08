2e4a506dc1a2617fa2b0b8347ff2c577c9f3f7de
Mood__Panda <andre-enju@ukr.net>
2023-11-08 22:00:19 +0200

    section testimonials

--- js/index.js
+++ js/index.js
@@ -8,4 +8,35 @@ burgerMenu.addEventListener("click", function () {
 
 btnClose.addEventListener("click", function () {
     navigationBurger.classList.remove("active");
-})
+});
+
+
+const slidesContainer = document.querySelector(".slides");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+
+let currentIndex = 0;
+
+nextBtn.addEventListener("click", () => {
+    if (currentIndex < 2) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    updateSlider();
+});
+
+prevBtn.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = 2;
+    }
+    updateSlider();
+});
+
+function updateSlider() {
+    const newTransformValue = `translateX(-${currentIndex * 100}vw)`;
+        slidesContainer.style.transform = newTransformValue;
+};
+
