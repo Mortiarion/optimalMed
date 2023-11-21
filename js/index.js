dc17cc07b6df9de9c475559234a17da4717f23ad
Mood__Panda <andre-enju@ukr.net>
2023-11-21 23:38:47 +0200

    section news new start((

--- js/index.js
+++ js/index.js
@@ -12,175 +12,175 @@ btnClose.addEventListener("click", function () {
 
 /**--------------------------------------------- */
 
-let cards = document.getElementsByClassName("card");
-  transforms = [
-    { x: 0, z: 0, scale: 1, filter: "blur(0px)" },
-    { x: "-105%", z: "-50px", scale: 0.999, filter: "blur(2.5px)" },
-    { x: "105%", z: "-50px", scale: 0.999, filter: "blur(2.5px)" },
-  ];
+// let cards = document.getElementsByClassName("card");
+//   transforms = [
+//     { x: 0, z: 0, scale: 1, filter: "blur(0px)" },
+//     { x: "-105%", z: "-50px", scale: 0.999, filter: "blur(2.5px)" },
+//     { x: "105%", z: "-50px", scale: 0.999, filter: "blur(2.5px)" },
+//   ];
 
-let nextTransform = function (x) {
-  if (x >= cards.length - 1) {
-    x = 0;
-  } else {
-    x++;
-  }
-  return x;
-};
+// let nextTransform = function (x) {
+//   if (x >= cards.length - 1) {
+//     x = 0;
+//   } else {
+//     x++;
+//   }
+//   return x;
+// };
 
-function next() {
-  for (i = 0; i < cards.length; i++) {
-    cards[i].style.transform =
-      "translateX(" +
-      transforms[nextTransform(i)].x +
-      ")" +
-      "translateZ(" +
-      transforms[nextTransform(i)].z +
-      ")" +
-      "scale(" +
-      transforms[nextTransform(i)].scale +
-      ")";
-    cards[i].style.filter = transforms[nextTransform(i)].filter;
-  }
-  transforms.push(transforms.shift());
-}
+// function next() {
+//   for (i = 0; i < cards.length; i++) {
+//     cards[i].style.transform =
+//       "translateX(" +
+//       transforms[nextTransform(i)].x +
+//       ")" +
+//       "translateZ(" +
+//       transforms[nextTransform(i)].z +
+//       ")" +
+//       "scale(" +
+//       transforms[nextTransform(i)].scale +
+//       ")";
+//     cards[i].style.filter = transforms[nextTransform(i)].filter;
+//   }
+//   transforms.push(transforms.shift());
+// }
 
-document.getElementById("carousel").onclick = function () {
-  next();
-};
+// document.getElementById("carousel").onclick = function () {
+//   next();
+// };
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-function updateCards() {
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].style.transform =
-      "translateX(" +
-      transforms[(i + currentIndex) % cards.length].x +
-      ")" +
-      "translateZ(" +
-      transforms[(i + currentIndex) % cards.length].z +
-      ")" +
-      "scale(" +
-      transforms[(i + currentIndex) % cards.length].scale +
-      ")";
-    cards[i].style.filter =
-      transforms[(i + currentIndex) % cards.length].filter;
-  }
-}
+// function updateCards() {
+//   for (let i = 0; i < cards.length; i++) {
+//     cards[i].style.transform =
+//       "translateX(" +
+//       transforms[(i + currentIndex) % cards.length].x +
+//       ")" +
+//       "translateZ(" +
+//       transforms[(i + currentIndex) % cards.length].z +
+//       ")" +
+//       "scale(" +
+//       transforms[(i + currentIndex) % cards.length].scale +
+//       ")";
+//     cards[i].style.filter =
+//       transforms[(i + currentIndex) % cards.length].filter;
+//   }
+// }
 
-function showNext() {
-  currentIndex = (currentIndex + 1) % cards.length;
-  updateCards();
-}
+// function showNext() {
+//   currentIndex = (currentIndex + 1) % cards.length;
+//   updateCards();
+// }
 
-function showPrev() {
-  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-  updateCards();
-}
+// function showPrev() {
+//   currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+//   updateCards();
+// }
 
-document.querySelector(".btn__next").addEventListener("click", showNext);
-document.querySelector(".btn__prev").addEventListener("click", showPrev);
+// document.querySelector(".btn__next").addEventListener("click", showNext);
+// document.querySelector(".btn__prev").addEventListener("click", showPrev);
 
-updateCards();
+// updateCards();
 
-
-/**--------------------------------------------- */
-
-let cardsTwo = document.getElementsByClassName("card__two");
-
-transformsTwo = [
-  { x: 0, z: 0, scale: 1, filter: "blur(0px)" },
-  { x: "-93%", z: "-50px", scale: 0.8, filter: "blur(2.5px)" },
-  { x: "93%", z: "-50px", scale: 0.8, filter: "blur(2.5px)" },
-];
-
-let nextTransformTwo = function (x) {
-  if (x >= cardsTwo.length - 1) {
-    x = 0;
-  } else {
-    x++;
-  }
-  return x;
-};
-
-function nextTwo() {
-  for (i = 0; i < cardsTwo.length; i++) {
-    cardsTwo[i].style.transform =
-      "translateX(" +
-      transformsTwo[nextTransformTwo(i)].x +
-      ")" +
-      "translateZ(" +
-      transformsTwo[nextTransformTwo(i)].z +
-      ")" +
-      "scale(" +
-      transformsTwo[nextTransformTwo(i)].scale +
-      ")";
-    cardsTwo[i].style.filter = transformsTwo[nextTransformTwo(i)].filter;
-  }
-  transformsTwo.push(transformsTwo.shift());
-}
-
-document.getElementById("carousel__two").onclick = function () {
-  nextTwo();
-};
-
-let currentIndexTwo = 0;
-
-function updateCardsTwo() {
-  for (let i = 0; i < cardsTwo.length; i++) {
-    cardsTwo[i].style.transform =
-      "translateX(" +
-      transformsTwo[(i + currentIndexTwo) % cardsTwo.length].x +
-      ")" +
-      "translateZ(" +
-      transformsTwo[(i + currentIndexTwo) % cardsTwo.length].z +
-      ")" +
-      "scale(" +
-      transformsTwo[(i + currentIndexTwo) % cardsTwo.length].scale +
-      ")";
-    cardsTwo[i].style.filter =
-      transformsTwo[(i + currentIndexTwo) % cardsTwo.length].filter;
-  }
-}
-
-function showNextTwo() {
-  currentIndexTwo = (currentIndexTwo + 1) % cardsTwo.length;
-  updateCardsTwo();
-}
-
-function showPrevTwo() {
-  currentIndexTwo = (currentIndexTwo - 1 + cardsTwo.length) % cardsTwo.length;
-  updateCardsTwo();
-}
-
-document.querySelector(".btn__next-two").addEventListener("click", showNextTwo);
-document.querySelector(".btn__prev-two").addEventListener("click", showPrevTwo);
-
-updateCardsTwo();
 
 /**--------------------------------------------- */
 
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// let cardsTwo = document.getElementsByClassName("card__two");
+
+// transformsTwo = [
+//   { x: 0, z: 0, scale: 1, filter: "blur(0px)" },
+//   { x: "-93%", z: "-50px", scale: 0.8, filter: "blur(2.5px)" },
+//   { x: "93%", z: "-50px", scale: 0.8, filter: "blur(2.5px)" },
+// ];
+
+// let nextTransformTwo = function (x) {
+//   if (x >= cardsTwo.length - 1) {
+//     x = 0;
+//   } else {
+//     x++;
+//   }
+//   return x;
+// };
+
+// function nextTwo() {
+//   for (i = 0; i < cardsTwo.length; i++) {
+//     cardsTwo[i].style.transform =
+//       "translateX(" +
+//       transformsTwo[nextTransformTwo(i)].x +
+//       ")" +
+//       "translateZ(" +
+//       transformsTwo[nextTransformTwo(i)].z +
+//       ")" +
+//       "scale(" +
+//       transformsTwo[nextTransformTwo(i)].scale +
+//       ")";
+//     cardsTwo[i].style.filter = transformsTwo[nextTransformTwo(i)].filter;
+//   }
+//   transformsTwo.push(transformsTwo.shift());
+// }
+
+// document.getElementById("carousel__two").onclick = function () {
+//   nextTwo();
+// };
+
+// let currentIndexTwo = 0;
+
+// function updateCardsTwo() {
+//   for (let i = 0; i < cardsTwo.length; i++) {
+//     cardsTwo[i].style.transform =
+//       "translateX(" +
+//       transformsTwo[(i + currentIndexTwo) % cardsTwo.length].x +
+//       ")" +
+//       "translateZ(" +
+//       transformsTwo[(i + currentIndexTwo) % cardsTwo.length].z +
+//       ")" +
+//       "scale(" +
+//       transformsTwo[(i + currentIndexTwo) % cardsTwo.length].scale +
+//       ")";
+//     cardsTwo[i].style.filter =
+//       transformsTwo[(i + currentIndexTwo) % cardsTwo.length].filter;
+//   }
+// }
+
+// function showNextTwo() {
+//   currentIndexTwo = (currentIndexTwo + 1) % cardsTwo.length;
+//   updateCardsTwo();
+// }
+
+// function showPrevTwo() {
+//   currentIndexTwo = (currentIndexTwo - 1 + cardsTwo.length) % cardsTwo.length;
+//   updateCardsTwo();
+// }
+
+// document.querySelector(".btn__next-two").addEventListener("click", showNextTwo);
+// document.querySelector(".btn__prev-two").addEventListener("click", showPrevTwo);
+
+// updateCardsTwo();
 
 /**--------------------------------------------- */
 
-const swiperTwo = new Swiper(".swiper-two", {
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: ".next",
-    prevEl: ".prev",
-  },
-});
+// const swiper = new Swiper(".swiper", {
+//   direction: "horizontal",
+//   loop: true,
+//   slidesPerView: 1,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+/**--------------------------------------------- */
+
+// const swiperTwo = new Swiper(".swiper-two", {
+//   direction: "horizontal",
+//   loop: true,
+//   slidesPerView: 1,
+//   navigation: {
+//     nextEl: ".next",
+//     prevEl: ".prev",
+//   },
+// });
 
 /**--------------------------------------------- */
 
