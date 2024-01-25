c246813f68a937a3eb2fb0caaa1c676a201b0b4d
Mood__Panda <andre-enju@ukr.net>
2024-01-25 23:05:03 +0200

    rework drop doctor form

--- js/index.js
+++ js/index.js
@@ -21,13 +21,12 @@ const toggleOverlay = () => {
 const toggleDropdownList = () => {
     dropdownList.classList.toggle("active-dropdown-list");
     disabledOrAnablesSroll();
-
 };
 
 const togglePopup = () => {
     popup.classList.toggle("active-popup");
     // disabledOrAnablesSroll();
-}
+};
 
 openPopup.forEach((element) => {
     element.addEventListener("click", () => {
@@ -44,7 +43,7 @@ openPopup.forEach((element) => {
 
 const disabledOrAnablesSroll = () => {
     document.body.classList.toggle("no-scroll-drop-popup");
-}
+};
 
 // const enableScroll = () => {
 //     document.body.classList.remove("");
@@ -73,12 +72,11 @@ document.addEventListener("click", (event) => {
             togglePopup();
             toggleOverlay();
             disabledOrAnablesSroll();
-        } 
+        }
 
         if (dropdownList.classList.contains("active-dropdown-list")) {
             toggleDropdownList();
             toggleOverlay();
-            
         }
         // else if (!popup.classList.contains("active-popup")) {
         //     toggleOverlay();
@@ -89,7 +87,7 @@ document.addEventListener("click", (event) => {
         //     togglePopup();
         // }
     }
-    
+
     if (isOpenPopup) {
         togglePopup();
     }
@@ -107,8 +105,64 @@ document.addEventListener("click", (event) => {
     //     togglePopup();
     //     toggleOverlay();
     // }
-
 });
+/**--------------------------------------------- */
+
+const checkBoxContainers = document.querySelectorAll(".checkbox-container");
+const selectButton = document.getElementById("select-button");
+const clearButton = document.getElementById("clear-btn");
+const selectedItems = [];
+
+clearButton.addEventListener("click", () => {
+    checkBoxContainers.forEach((container) => {
+        const checkBox = container.querySelector("input[type='checkbox']");
+        checkBox.checked = false; // Скидання стану чекбокса
+    });
+    selectedItems.length = 0; // Очищення масиву вибраних елементів
+    updateSelectedCount(); // Оновлення кількості вибраних елементів
+});
+
+checkBoxContainers.forEach((container) => {
+    const checkBox = container.querySelector("input[type='checkbox']");
+
+    container.addEventListener("change", () => {
+        if (checkBox.checked) {
+            selectedItems.push(checkBox.value);
+        } else {
+            const index = selectedItems.indexOf(checkBox.value);
+            if (index !== -1) {
+                selectedItems.splice(index, 1);
+            }
+        }
+        updateSelectedCount();
+    });
+});
+
+selectButton.addEventListener("click", () => {
+    // Отримання посилань на елементи dropdownList та overlay
+    const dropdownList = document.querySelector(".dropdown-list");
+    const overlay = document.querySelector(".overlay");
+
+    dropdownList.classList.remove("active-dropdown-list");
+    overlay.classList.remove("active-overlay");
+
+    if (document.body.classList.contains("no-scroll-drop-popup")) {
+        disabledOrAnablesSroll();
+    }
+    updateSelectedCount();
+});
+
+// Оголошення функції updateSelectedCount
+const updateSelectedCount = () => {
+    const count = selectedItems.length;
+    let selectedText = `выбрано ${count} специализаций`;
+    if (count === 0) {
+        selectedText = ""; // робим пустим якщо кількість знову стає - 0 спеціалізацій
+    }
+    const selectedCountElement = document.querySelector(".number-selected");
+    selectedCountElement.textContent = selectedText;
+};
+
 /**--------------------------------------------- */
 
 // let cards = document.getElementsByClassName("card");
@@ -625,122 +679,123 @@ document.addEventListener("click", (event) => {
 
 /**--------------------------------------------- */
 
-// отримуєм доступ до елементів які нам потрібні в подальшому
-const checkBoxContainers = document.querySelectorAll(".checkbox-container");
-const selectedSpecializations = document.querySelector(
-    ".selected-specializations"
-);
-const selectButton = document.getElementById("select-button");
-const selectedItems = [];
+// // отримуєм доступ до елементів які нам потрібні в подальшому
+// const checkBoxContainers = document.querySelectorAll(".checkbox-container");
+// const selectedSpecializations = document.querySelector(
+//     ".selected-specializations"
+// );
+// const selectButton = document.getElementById("select-button");
+// const selectedItems = [];
+// console.log(selectedItems)
 
-// forEach ми використувуєм щоб пребрати весь список доступ них елементом
-checkBoxContainers.forEach((container) => {
-    const checkBox = container.querySelector("input[type='checkbox']"); // знаходим чекбокс в контейнерах
-    const text = container.textContent.trim(); // знаходим текст і видаляєм пробіли щоб не листати кілометрове полотно
+// // forEach ми використувуєм щоб пребрати весь список доступ них елементом
+// checkBoxContainers.forEach((container) => {
+//     // console.log(container)
+//     const checkBox = container.querySelector("input[type='checkbox']"); // знаходим чекбокс в контейнерах
+//     const text = container.textContent.trim(); // знаходим текст і видаляєм пробіли щоб не листати кілометрове полотно
+// // console.log(text);
 
-    container.addEventListener("change", () => { // прослуховувач подій на зміну стану. на майбутнє в випадках селекта чекбокса або радіо кнопок. ато замахався понімати чого не робили з cklick
-        if (checkBox.checked) { // провірка чи чекбоес активний
-            if (!selectedItems.includes(text)) { // Якщо чекбокс відзначений і текст ще не знаходиться серед обраних елементів
-                selectedItems.push(text); // Додаємо текст до масиву обраних елементів
+//     container.addEventListener("change", () => { // прослуховувач подій на зміну стану. на майбутнє в випадках селекта чекбокса або радіо кнопок. ато замахався понімати чого не робили з cklick
+//         if (checkBox.checked) { // провірка чи чекбоес активний
+//             if (!selectedItems.includes(text)) { // Якщо чекбокс відзначений і текст ще не знаходиться серед обраних елементів
+//                 selectedItems.push(text); // Додаємо текст до масиву обраних елементів
+// console.log(selectedItems);
 
-                const span = document.createElement("span"); // створення елементу куди будем поміщати дані і далі кнопку видалення
-                span.classList.add("checked-element");
-                span.textContent = text;
-                span.dataset.value = text; // Додаємо атрибут зі значенням для пошуку
+//                 // const span = document.createElement("span"); // створення елементу куди будем поміщати дані і далі кнопку видалення
+//                 // span.classList.add("checked-element");
+//                 // span.textContent = text;
+//                 // span.dataset.value = text; // Додаємо атрибут зі значенням для пошуку
 
+//                 // const deleteBtn = document.createElement("button"); // це та кнопка виделення
+//                 // deleteBtn.classList.add("delete-btn");
+//                 // deleteBtn.innerHTML = "&#10006";
 
-                const deleteBtn = document.createElement("button"); // це та кнопка виделення
-                deleteBtn.classList.add("delete-btn");
-                deleteBtn.innerHTML = "&#10006";
+//                 // span.appendChild(deleteBtn);
 
-                span.appendChild(deleteBtn);
+//                 // selectedSpecializations.appendChild(span);
+//             }
+//         } else {
+//             // якщо чекбокс занятий
+//             const index = selectedItems.indexOf(text);
+//             // console.log(index);
+//             if (index !== -1) {
+//                 selectedItems.splice(index, 1); // видаляєм текст з масиву обраних елементів
 
-                selectedSpecializations.appendChild(span);
-            }
-        } else {
-            // якщо чекбокс занятий
-            const index = selectedItems.indexOf(text);
-            // console.log(index);
-            if (index !== -1) {
-                selectedItems.splice(index, 1); // видаляєм текст з масиву обраних елементів
+//                 // Знаходимо вибраний елемент у відображенні вибраних спеціалізацій та видаляємо його
+//                 const spanToRemove = selectedSpecializations.querySelector(
+//                     `span[data-value="${text}"]`
+//                 );
+//                 console.log(spanToRemove);
+//                 if (spanToRemove) {
+//                     spanToRemove.remove();
+//                 }
+//             }
+//         }
+//         updateSelectedCount();
+//     });
 
-                // Знаходимо вибраний елемент у відображенні вибраних спеціалізацій та видаляємо його
-                const spanToRemove = selectedSpecializations.querySelector(
-                    `span[data-value="${text}"]`
-                );
-                // console.log(spanToRemove);
-                if (spanToRemove) {
-                    spanToRemove.remove();
-                }
-            }
-        }
-        updateSelectedCount();
-    });
+//     // selectedSpecializations.addEventListener("click", (event) => {
+//     //     if (event.target.classList.contains("delete-btn")) {
+//     //         const span = event.target.parentElement;
+//     //         // console.log(span);
+//     //         const specializationText = span.getAttribute("data-value");
+//     //         // console.log(specializationText);
+//     //         const index = selectedItems.indexOf(specializationText);
+//     //         if (index !== -1) {
+//     //             selectedItems.splice(index, 1);
+//     //         }
+//     //         span.remove();
 
-    selectedSpecializations.addEventListener("click", (event) => {
-        if (event.target.classList.contains("delete-btn")) {
-            const span = event.target.parentElement;
-            // console.log(span);
-            const specializationText = span.getAttribute("data-value");
-            // console.log(specializationText);
-            const index = selectedItems.indexOf(specializationText);
-            if (index !== -1) {
-                selectedItems.splice(index, 1);
-            }
-            span.remove();
+//     //         // Знайти відповідний чекбокс та скасувати опцію "checked"
+//     //         checkBoxContainers.forEach((container) => {
+//     //             const checkBox = container.querySelector(
+//     //                 "input[type='checkbox']"
+//     //             );
+//     //         });
+//     //         if (container.textContent.trim() === specializationText) {
+//     //             checkBox.checked = false;
+//     //         }
+//     //         updateSelectedCount();
+//     //     }
+//     // });
 
-            // Знайти відповідний чекбокс та скасувати опцію "checked"
-            checkBoxContainers.forEach((container) => {
-                const checkBox = container.querySelector(
-                    "input[type='checkbox']"
-                );
-            });
-            if (container.textContent.trim() === specializationText) {
-                checkBox.checked = false;
-            }
-            updateSelectedCount();
-        }
-    });
+//     selectButton.addEventListener("click", () => {
+//         // selectedItems.length = 0;
+//         // selectedSpecializations.textContent = "";
 
-    selectButton.addEventListener("click", () => {
-        // selectedItems.length = 0;
-        // selectedSpecializations.textContent = "";
+//         // checkBoxContainers.forEach((container) => {
+//         //     const checkBox = container.querySelector("input[type='checkbox']");
+//         //     checkBox.checked = false;
+//         // });
+//         // console.log(selectButton);
 
-        // checkBoxContainers.forEach((container) => {
-        //     const checkBox = container.querySelector("input[type='checkbox']");
-        //     checkBox.checked = false;
-        // });
-        console.log(selectButton);
+//         dropdownList.classList.remove("active-dropdown-list");
+//         overlay.classList.remove("active-overlay");
+//         // disabledOrAnablesSroll();
+//         // document.body.classList.remove("no-sroll-drop-popup")
+//         if (document.body.classList.contains("no-scroll-drop-popup")) {
+//             disabledOrAnablesSroll();
+//         }
+//             updateSelectedCount();
+//     });
+// });
 
-        dropdownList.classList.remove("active-dropdown-list");
-        overlay.classList.remove("active-overlay");
-        // disabledOrAnablesSroll();
-        // document.body.classList.remove("no-sroll-drop-popup")
-        if (document.body.classList.contains("no-scroll-drop-popup")) {
-            disabledOrAnablesSroll();
-        }
-            updateSelectedCount();
-    });
-});
-
-
-const updateSelectedCount = () => {
-    const count = selectedItems.length;
-    let selectedText = `выбрано ${count} специализаций`;
-    // if (selectedText === "") {
-    //     selectedText.textContent = "";
-    // }
-    if (count === 0) {
-        selectedText = ""; // робим пустим якщо кількість знову стає - 0 спеціалізацій
-    }
-    // Ваш елемент, де ви хочете відображати кількість вибраних елементів
-    const selectedCountElement = document.querySelector(".number-selected");
-    selectedCountElement.textContent = selectedText;
-    // disabledOrAnablesSroll();
-}
+// const updateSelectedCount = () => {
+//     const count = selectedItems.length;
+//     let selectedText = `выбрано ${count} специализаций`;
+//     // if (selectedText === "") {
+//     //     selectedText.textContent = "";
+//     // }
+//     if (count === 0) {
+//         selectedText = ""; // робим пустим якщо кількість знову стає - 0 спеціалізацій
+//     }
+//     // Ваш елемент, де ви хочете відображати кількість вибраних елементів
+//     const selectedCountElement = document.querySelector(".number-selected");
+//     selectedCountElement.textContent = selectedText;
+//     // disabledOrAnablesSroll();
+// }
 
 /**--------------------------------------------- */
-
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     const checkboxContainers = document.querySelectorAll(".checkbox-container");
@@ -819,3 +874,5 @@ const updateSelectedCount = () => {
 //         });
 //     });
 // });
+
+/**--------------------------------------------- */
---
+++ none/none.js
@@ -0,0 +1,118 @@
+// отримуєм доступ до елементів які нам потрібні в подальшому
+const checkBoxContainers = document.querySelectorAll(".checkbox-container");
+const selectedSpecializations = document.querySelector(
+    ".selected-specializations"
+);
+const selectButton = document.getElementById("select-button");
+const selectedItems = [];
+console.log(selectedItems);
+
+// forEach ми використувуєм щоб пребрати весь список доступ них елементом
+checkBoxContainers.forEach((container) => {
+    // console.log(container)
+    const checkBox = container.querySelector("input[type='checkbox']"); // знаходим чекбокс в контейнерах
+    const text = container.textContent.trim(); // знаходим текст і видаляєм пробіли щоб не листати кілометрове полотно
+    // console.log(text);
+
+    container.addEventListener("change", () => {
+        // прослуховувач подій на зміну стану. на майбутнє в випадках селекта чекбокса або радіо кнопок. ато замахався понімати чого не робили з cklick
+        if (checkBox.checked) {
+            // провірка чи чекбоес активний
+            if (!selectedItems.includes(text)) {
+                // Якщо чекбокс відзначений і текст ще не знаходиться серед обраних елементів
+                selectedItems.push(text); // Додаємо текст до масиву обраних елементів
+                console.log(selectedItems);
+
+                // const span = document.createElement("span"); // створення елементу куди будем поміщати дані і далі кнопку видалення
+                // span.classList.add("checked-element");
+                // span.textContent = text;
+                // span.dataset.value = text; // Додаємо атрибут зі значенням для пошуку
+
+                // const deleteBtn = document.createElement("button"); // це та кнопка виделення
+                // deleteBtn.classList.add("delete-btn");
+                // deleteBtn.innerHTML = "&#10006";
+
+                // span.appendChild(deleteBtn);
+
+                // selectedSpecializations.appendChild(span);
+            }
+        } else {
+            // якщо чекбокс занятий
+            const index = selectedItems.indexOf(text);
+            // console.log(index);
+            if (index !== -1) {
+                selectedItems.splice(index, 1); // видаляєм текст з масиву обраних елементів
+
+                // Знаходимо вибраний елемент у відображенні вибраних спеціалізацій та видаляємо його
+                const spanToRemove = selectedSpecializations.querySelector(
+                    `span[data-value="${text}"]`
+                );
+                console.log(spanToRemove);
+                if (spanToRemove) {
+                    spanToRemove.remove();
+                }
+            }
+        }
+        updateSelectedCount();
+    });
+
+    // selectedSpecializations.addEventListener("click", (event) => {
+    //     if (event.target.classList.contains("delete-btn")) {
+    //         const span = event.target.parentElement;
+    //         // console.log(span);
+    //         const specializationText = span.getAttribute("data-value");
+    //         // console.log(specializationText);
+    //         const index = selectedItems.indexOf(specializationText);
+    //         if (index !== -1) {
+    //             selectedItems.splice(index, 1);
+    //         }
+    //         span.remove();
+
+    //         // Знайти відповідний чекбокс та скасувати опцію "checked"
+    //         checkBoxContainers.forEach((container) => {
+    //             const checkBox = container.querySelector(
+    //                 "input[type='checkbox']"
+    //             );
+    //         });
+    //         if (container.textContent.trim() === specializationText) {
+    //             checkBox.checked = false;
+    //         }
+    //         updateSelectedCount();
+    //     }
+    // });
+
+    selectButton.addEventListener("click", () => {
+        // selectedItems.length = 0;
+        // selectedSpecializations.textContent = "";
+
+        // checkBoxContainers.forEach((container) => {
+        //     const checkBox = container.querySelector("input[type='checkbox']");
+        //     checkBox.checked = false;
+        // });
+        // console.log(selectButton);
+
+        dropdownList.classList.remove("active-dropdown-list");
+        overlay.classList.remove("active-overlay");
+        // disabledOrAnablesSroll();
+        // document.body.classList.remove("no-sroll-drop-popup")
+        if (document.body.classList.contains("no-scroll-drop-popup")) {
+            disabledOrAnablesSroll();
+        }
+        updateSelectedCount();
+    });
+});
+
+const updateSelectedCount = () => {
+    const count = selectedItems.length;
+    let selectedText = `выбрано ${count} специализаций`;
+    // if (selectedText === "") {
+    //     selectedText.textContent = "";
+    // }
+    if (count === 0) {
+        selectedText = ""; // робим пустим якщо кількість знову стає - 0 спеціалізацій
+    }
+    // Ваш елемент, де ви хочете відображати кількість вибраних елементів
+    const selectedCountElement = document.querySelector(".number-selected");
+    selectedCountElement.textContent = selectedText;
+    // disabledOrAnablesSroll();
+};
