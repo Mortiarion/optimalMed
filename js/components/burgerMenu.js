4552e58b67749b2993f5a677601dc05385a58dab
Mood__Panda <andre-enju@ukr.net>
2024-02-05 21:42:41 +0200

    fix

--- js/components/burgerMenu.js
+++ js/components/burgerMenu.js
@@ -4,10 +4,10 @@ const btnClose = document.querySelector(".btn__close");
 
 burgerMenu.addEventListener("click", function () {
     navigationBurger.classList.add("active");
-    document.body.classList.add("no-scroll-drop-popup");
+    // document.body.classList.add("no-scroll-drop-popup");
 });
 
 btnClose.addEventListener("click", function () {
     navigationBurger.classList.remove("active");
-    document.body.classList.remove("no-scroll-drop-popup");
+    // document.body.classList.remove("no-scroll-drop-popup");
 });
